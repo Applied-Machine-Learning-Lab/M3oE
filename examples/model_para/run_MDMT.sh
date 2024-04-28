53e01842372619e30a74aa999195d44a814df1b7
Zhang-Zijian <131531440+Zhang-Zijian@users.noreply.github.com>
2024-04-28 21:39:23 +0800

    Delete examples/model_para/run_MDMT.sh

--- examples/model_para/run_MDMT.sh
+++
@@ -1,5 +0,0 @@
-python run_movielens.py --seed 2022 
-python run_movielens.py --seed 2023 
-python run_movielens.py --seed 2024 
-python run_movielens.py --seed 2025 
-python run_movielens.py --seed 2026
